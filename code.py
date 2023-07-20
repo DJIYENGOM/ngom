5fba99982d71f832c3e5b2e8bacf4d06164c7e7a
DJIYENGOM <ngomdjiye@gmail.com>
2023-07-20 12:25:11 +0000

    Ajout du fichier code.py

---
+++ code.py
@@ -0,0 +1,10 @@
+# code.py
+
+def somme_deux_nombres(a, b):
+    return a + b
+
+# Exemple d'utilisation
+nombre1 = 10
+nombre2 = 5
+resultat = somme_deux_nombres(nombre1, nombre2)
+print(f"La somme de {nombre1} et {nombre2} est égale à : {resultat}")
